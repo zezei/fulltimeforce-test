71e17dde1b25fe259544245deae2cfa0a707687f
zezei <catgolondrinas@gmail.com>
2023-03-14 17:28:44 -0300

    wip: #02 resources generated for github logic interactions

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { GithubModule } from './github/github.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    GithubModule,
   ],
   providers: [],
 })
 export class AppModule {}
+
+
---
+++ backend/src/github/github.controller.ts
@@ -0,0 +1,7 @@
+import { Controller } from '@nestjs/common';
+import { GithubService } from './github.service';
+
+@Controller('github')
+export class GithubController {
+  constructor(private readonly githubService: GithubService) {}
+}
