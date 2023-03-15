c4045b914875d05128ef6a2265f1e19b0ae277ae
zezei <catgolondrinas@gmail.com>
2023-03-14 22:54:09 -0300

    feat: closes #02 fetching and returning commits from github repository (hardcoded)

--- backend/src/github/github.module.ts
+++ backend/src/github/github.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { GithubService } from './github.service';
 import { GithubController } from './github.controller';
 import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   controllers: [GithubController],
   providers: [GithubService],
-  imports: [ConfigModule]
+  imports: [ConfigModule, HttpModule]
 })
 export class GithubModule {}
--- backend/src/github/github.service.ts
+++ backend/src/github/github.service.ts
@@ -1,12 +1,44 @@
+import { HttpService } from '@nestjs/axios/dist';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { map } from 'rxjs/operators';
+export interface Committer {
+  name: string;
+  email: string;
+  date: Date;
+}
+export interface Commit {
+  message: string;
+  url: string;
+  comment_count: number;
+  committer: Committer;
 
+}
+export interface GitHubResponse {
+  sha: string;
+  node_id: string;
+  commit: Commit;
+}
 @Injectable()
 export class GithubService {
-
-    COMMITS_URL = this.configService.get<string>('GITHUB_ENDPOINT')
-    constructor(private configService: ConfigService) {}
-    fetchCommits() { 
-    return null;
+  COMMITS_URL = this.configService.get<string>('GITHUB_ENDPOINT');
+  constructor(
+    private configService: ConfigService,
+    private readonly httpService: HttpService,
+  ) {}
+  fetchCommits() {
+    return this.httpService.get(this.COMMITS_URL).pipe(
+      //Get the data from the body
+      map((response) => response['data']),
+      //Map the response
+      map((githubResponse: GitHubResponse[]) =>
+        githubResponse.map((res) => ({
+          message: res.commit.message,
+          id: res.sha,
+          author: res.commit.committer.email,
+          date: res.commit.committer.date,
+        })),
+      ),
+    );
   }
 }
